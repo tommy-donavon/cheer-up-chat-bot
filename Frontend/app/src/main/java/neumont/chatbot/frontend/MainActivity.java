f6627e4197bca8ccd42ebf86d49844bf06237916
Odin34 <57544047+Odin34@users.noreply.github.com>
2021-02-17 14:28:31 -1000

    Added texview to test connectivity with backend bot

--- Frontend/app/src/main/java/neumont/chatbot/frontend/MainActivity.java
+++ Frontend/app/src/main/java/neumont/chatbot/frontend/MainActivity.java
@@ -3,12 +3,30 @@ package neumont.chatbot.frontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username, password;
+    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = findViewById(R.id.etUsername);
+        password = findViewById(R.id.etPassword);
+        test = findViewById(R.id.tvTestOutput);
+    }
+
+    public void continueAsGuestOnClick(View view) {
+    }
+
+    public void signInOnClick(View view) {
+    }
+
+    public void signUpOnClick(View view) {
     }
 }
