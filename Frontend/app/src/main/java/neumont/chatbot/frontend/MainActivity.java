c85c197422edf51cb8074c24f0ef7aa470c96ca1
Odin34 <57544047+Odin34@users.noreply.github.com>
2021-02-17 14:47:20 -1000

    Added Intent object to the login guest button to switch screens

--- Frontend/app/src/main/java/neumont/chatbot/frontend/MainActivity.java
+++ Frontend/app/src/main/java/neumont/chatbot/frontend/MainActivity.java
@@ -2,6 +2,7 @@ package neumont.chatbot.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void continueAsGuestOnClick(View view) {
+        Intent intent = new Intent(this, ChatScreen.class);
+        startActivity(intent);
     }
 
     public void signInOnClick(View view) {
